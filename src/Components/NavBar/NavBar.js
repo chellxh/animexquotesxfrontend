5fb6b7fdb616d5ddb343b7819576122a62ad7df3
Michelle Harley <chell.hx@gmail.com>
2023-08-24 11:01:08 -0400

    edit dropdown class in nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -13,19 +13,11 @@ function NavBar() {
       </div>
       <div className="dropdown">
         <Button className={"button-dropbtn"} value={"Categories"} id={10} />
-        <div class="dropdown-content">
+        <div className="dropdown-content">
           <Link to="/about">About</Link>
           <Link to="/shows">Shows</Link>
           <Link to="/characters">Characters</Link>
           <Link to="/quotes">Quotes</Link>
-        </div>
-      </div>
-      <div className="dropdown">
-        <Button className={"button-dropbtn"} value={"Create New"} id={5} />
-        <div className="dropdown-content">
-          <a href="/shows/new">Show</a>
-          <a href="/characters/new">Character</a>
-          <a href="/quotes/new">Quote</a>
         </div>
       </div>
     </nav>
